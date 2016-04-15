4fde9f3209d402e270773bfbea0f8cf695034fb7
Aurore <Aurore@172.17.42.165>
2016-04-15 15:20:18 +0200

    persistence

---
+++ generate/src/Persistence/Porte.java
@@ -0,0 +1,32 @@
+package Persistence;
+
+public class Porte {
+
+	private int idporte;
+	private String situationporte;
+	private int idpiece;
+	
+	public int getIdporte() {
+		return idporte;
+	}
+	
+	public void setIdporte(int idporte) {
+		this.idporte = idporte;
+	}
+	
+	public String getSituationporte() {
+		return situationporte;
+	}
+	
+	public void setSituationporte(String situationporte) {
+		this.situationporte = situationporte;
+	}
+	
+	public int getIdpiece() {
+		return idpiece;
+	}
+	
+	public void setIdpiece(int idpiece) {
+		this.idpiece = idpiece;
+	}
+}
