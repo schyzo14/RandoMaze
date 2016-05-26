cfb6da63c45feac51ca1652ec013e094443d90d8
Aurore <aurore.queille@free.fr>
2016-05-26 11:33:22 +0200

    [Inscription] fermer fenetre / quitter

--- generate/src/ui/Inscription.java
+++ generate/src/ui/Inscription.java
@@ -8,10 +8,13 @@ package ui;
 import java.awt.Dimension;
 import java.awt.Insets;
 import java.awt.Toolkit;
+import java.awt.Window;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+
+import javax.swing.JFrame;
 
 import model.Labyrinthe;
 import model.LabyrintheImpl;
@@ -58,7 +61,7 @@ public class Inscription extends javax.swing.JFrame {
         motDePasse = new javax.swing.JPasswordField();
         buttonInscription = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        //setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("RandoMaze - Inscription");
         setPreferredSize(new java.awt.Dimension(190, 218));
 
@@ -110,6 +113,19 @@ public class Inscription extends javax.swing.JFrame {
             }
         });
 
+        // Croix fermer
+        this.addWindowListener(new java.awt.event.WindowAdapter() { 
+        	public void windowClosing(java.awt.event.WindowEvent evt) { 
+        		try {
+					Labyrinthe laby = (Labyrinthe) Naming.lookup("MonServeur1");
+					quitterFenetre();
+				} catch (MalformedURLException | RemoteException | NotBoundException e) {
+					e.printStackTrace();
+				}
+        		
+            }
+        });
+        
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -201,6 +217,23 @@ public class Inscription extends javax.swing.JFrame {
         
     }//GEN-LAST:event_buttonInscriptionMouseClicked
 
+    public void quitterFenetre() throws RemoteException, MalformedURLException, NotBoundException {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		// tester s'il y a d'autres fenetre ouvertes
+		Window[] frames = JFrame.getWindows();
+		boolean ouvert = false;
+		for (int i=0 ; i<frames.length ; i++) {
+			if (frames[i].isVisible() && ! this.getName().equals(frames[i].getName())){
+				ouvert = true;
+			}
+		}
+		if (!ouvert) {
+			Labyrinthe laby = (Labyrinthe) Naming.lookup("MonServeur1");
+			laby.quitter();
+		}
+    }
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton buttonInscription;
     private javax.swing.JLabel jLabel1;
