04c678ac3ba6c60c2bd43bea0237dd3f8b35ac2c
Aurore <aurore.queille@free.fr>
2016-05-29 21:34:22 +0200

    [interface LabyrintheJDBC] Commentaires

--- generate/src/persistence/Labyrinthe.java
+++ generate/src/persistence/Labyrinthe.java
@@ -11,45 +11,137 @@ import model.Utilisateur;
 /**
  * Interface de LabyrintheJDBC
  * 
- * @author Aurore
- *
+ * @author Manon, Aurore, Youssef
  */
 public interface Labyrinthe extends java.rmi.Remote {
 
-	// Pièces
-	public ArrayList<Piece> selectPiece () 
-			throws java.rmi.RemoteException;
-	
-	public Piece selectPieceById(int idPiece)
-			throws java.rmi.RemoteException;
+	// Pièce
+	/**
+	 * Liste de toutes les pieces
+	 * 
+	 * @return liste des pieces
+	 * @throws java.rmi.RemoteException
+	 */
+	public ArrayList<Piece> selectPiece() throws java.rmi.RemoteException;
+
+	/**
+	 * Retourne une piece grace à son id
+	 * 
+	 * @param idPiece
+	 * @return piece
+	 * @throws java.rmi.RemoteException
+	 */
+	public Piece selectPieceById(int idPiece) throws java.rmi.RemoteException;
 
 	// Porte
-	public ArrayList<Porte> selectPorteByIdPiece (int idpiece)
+	/**
+	 * Retourne la liste des portes d'une piece
+	 * 
+	 * @param idpiece
+	 * @return liste des portes
+	 * @throws java.rmi.RemoteException
+	 */
+	public ArrayList<Porte> selectPorteByIdPiece(int idpiece)
 			throws java.rmi.RemoteException;
-	
+
 	// Utilisateur
-	public Utilisateur selectUtilisateurByNom (String nom)
+	/**
+	 * Retourne un utilisateur grave à son nom
+	 * 
+	 * @param nom
+	 * @return Utilisateur
+	 * @throws java.rmi.RemoteException
+	 */
+	public Utilisateur selectUtilisateurByNom(String nom)
 			throws java.rmi.RemoteException;
-	public boolean creerUtilisateur (String nom, String mdp)
+
+	/**
+	 * Créer un utilisateur avec son nom et mdp
+	 * 
+	 * @param nom
+	 * @param mdp
+	 * @return false : le nom existe déjà / true : création réussie
+	 * @throws java.rmi.RemoteException
+	 */
+	public boolean creerUtilisateur(String nom, String mdp)
 			throws java.rmi.RemoteException;
-	
+
 	// Personnage
-	public ArrayList<Personnage> selectPersonnageByUtilisateur (int idUtilisateur)
+	/**
+	 * Retourne les personnages d'un utilisateur
+	 * 
+	 * @param idUtilisateur
+	 * @return liste de personnages
+	 * @throws java.rmi.RemoteException
+	 */
+	public ArrayList<Personnage> selectPersonnageByUtilisateur(int idUtilisateur)
 			throws java.rmi.RemoteException;
-	public Personnage selectPersonnageByName (String nomPerso)
+
+	/**
+	 * Retourne un personnage grace à son nom
+	 * 
+	 * @param nomPerso
+	 * @return personnage
+	 * @throws java.rmi.RemoteException
+	 */
+	public Personnage selectPersonnageByName(String nomPerso)
 			throws java.rmi.RemoteException;
-	public boolean creerPersonnage (String nom ,int idUtilisateur)
+
+	/**
+	 * Créer un personnage avec son nom et utilisateur
+	 * 
+	 * @param nom
+	 * @param idUtilisateur
+	 * @return false : le nom existe déjà / true : création réussie
+	 * @throws java.rmi.RemoteException
+	 */
+	public boolean creerPersonnage(String nom, int idUtilisateur)
 			throws java.rmi.RemoteException;
-	public boolean updatePersonnage (int id, String nom ,int pointvie, int idpiece)
-			throws java.rmi.RemoteException;
+
+	/**
+	 * Mettre à jour un personnage
+	 * 
+	 * @param id
+	 * @param nom
+	 * @param pointvie
+	 * @param idpiece
+	 * @return false : erreur / true : MAJ réussie
+	 * @throws java.rmi.RemoteException
+	 */
+	public boolean updatePersonnage(int id, String nom, int pointvie,
+			int idpiece) throws java.rmi.RemoteException;
+
 	
 	// Monstre
-	public Monstre selectMonstreByPiece (int idPiece)
+	/**
+	 * Retourne le monstre d'une piece
+	 * 
+	 * @param idPiece
+	 * @return monstre
+	 * @throws java.rmi.RemoteException
+	 */
+	public Monstre selectMonstreByPiece(int idPiece)
 			throws java.rmi.RemoteException;
-	public boolean updateMonstre (int id, String nom ,int pointvie, int idpiece)
+
+	/**
+	 * Mettre à jour un monstre
+	 * 
+	 * @param id
+	 * @param nom
+	 * @param pointvie
+	 * @param idpiece
+	 * @return false : erreur / true : MAJ réussie
+	 * @throws java.rmi.RemoteException
+	 */
+	public boolean updateMonstre(int id, String nom, int pointvie, int idpiece)
 			throws java.rmi.RemoteException;
+
 	
 	// Fermeture
-	public void fermer() 
-			throws Exception;
+	/**
+	 * fermer l'accès à la bd
+	 * 
+	 * @throws Exception
+	 */
+	public void fermer() throws Exception;
 }
