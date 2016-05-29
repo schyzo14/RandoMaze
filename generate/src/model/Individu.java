6f3f611f5eea80bde5615f52988b9f3c58e839b8
Schyzo <Schyzo@Schyzo-PC>
2016-05-29 21:23:12 +0200

    [Individu] Correction sleep

--- generate/src/model/Individu.java
+++ generate/src/model/Individu.java
@@ -80,7 +80,7 @@ public class Individu implements Serializable {
 		
 		// attendre 1 seconde
 		try {
-		    Thread.sleep(3000);             // 1000 milliseconds is one second.
+		    Thread.sleep(1000);             // 1000 milliseconds is one second.
 		} catch(InterruptedException ex) {
 		    Thread.currentThread().interrupt();
 		}
